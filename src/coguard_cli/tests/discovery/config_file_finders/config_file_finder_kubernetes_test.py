e5c5807c34e9cb94b5c84c2bdd8c63da4ea5df2e
Albert Heinle <albert.heinle@googlemail.com>
2023-02-12 22:35:22 -0500

    fixed the tests

--- src/coguard_cli/tests/discovery/config_file_finders/config_file_finder_kubernetes_test.py
+++ src/coguard_cli/tests/discovery/config_file_finders/config_file_finder_kubernetes_test.py
@@ -75,9 +75,9 @@ class TestConfigFileFinderKubernetes(unittest.TestCase):
                 "os.walk",
                 new_callable=lambda: lambda location: [("etc", [], ["kubernetes.yaml"])]), \
                 unittest.mock.patch(
-                    ("coguard_cli.discovery.config_file_finders.create_temp_"
-                     "location_and_manifest_entry"),
-                    new_callable=lambda: lambda a, b, c, d, e, f: ({"foo": "bar"}, "/etc/bar")
+                    ("coguard_cli.discovery.config_file_finders.create_grouped_temp_"
+                     "locations_and_manifest_entries"),
+                    new_callable=lambda: lambda a, b, c, d, e: [({"foo": "bar"}, "/etc/bar")]
                 ), \
                 unittest.mock.patch(
                     ("coguard_cli.discovery.config_file_finders."
@@ -101,9 +101,9 @@ class TestConfigFileFinderKubernetes(unittest.TestCase):
                 "os.walk",
                 new_callable=lambda: lambda location: [("etc", [], ["kubernetes.yml"])]), \
                 unittest.mock.patch(
-                    ("coguard_cli.discovery.config_file_finders.create_temp_"
-                     "location_and_manifest_entry"),
-                    new_callable=lambda: lambda a, b, c, d, e, f: ({"foo": "bar"}, "/etc/bar")
+                    ("coguard_cli.discovery.config_file_finders.create_grouped_temp_"
+                     "locations_and_manifest_entries"),
+                    new_callable=lambda: lambda a, b, c, d, e: [({"foo": "bar"}, "/etc/bar")]
                 ), \
                 unittest.mock.patch(
                     ("coguard_cli.discovery.config_file_finders."
