1d0a32794096fd4330c6da272b297cedb98a8f9b
Albert Heinle <albert.heinle@googlemail.com>
2023-09-06 08:50:41 -0400

    PR comments

--- src/coguard_cli/discovery/cloud_discovery/terraformer_extract_image_helper/data/versions.tf
+++ src/coguard_cli/discovery/cloud_discovery/terraformer_extract_image_helper/data/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      # This is a temporary fixing of the version, until https://github.com/GoogleCloudPlatform/terraformer/issues/1695 is fixed
       version = "4.59.0"
     }
     aws = {
