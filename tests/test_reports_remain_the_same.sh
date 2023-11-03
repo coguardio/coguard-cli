31089ee00cca0e60ec6f8c08ab00ea7e9661a471
Albert Heinle <albert.heinle@googlemail.com>
2023-11-03 12:40:33 -0400

    Figured it out (I think ;-))

--- tests/test_reports_remain_the_same.sh
+++ tests/test_reports_remain_the_same.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 test -n "$COGUARD_USER_NAME"
 test -n "$COGUARD_PASSWORD"
@@ -32,35 +32,35 @@ test_folder_checksum() {
 
 # Docker image tests
 
-test_image_checksum "nginx:1.23.2" "9e0feaad74d6f67088f37d64b0cb16ee3fc4ab81"
+test_image_checksum "nginx:1.23.2" "e1ccb91d20bcee9271b0546e4f7499da8b1deb4e"
 docker image rm "nginx:1.23.2"
-test_image_checksum "mysql:8.0.31" "d5a8a531da1040529d5b94857487037ac238e3d6"
+test_image_checksum "mysql:8.0.31" "ab23c1f34b955ee1b429fdf098330e9cdd8dde01"
 docker image rm "mysql:8.0.31"
-test_image_checksum "postgres:15.1" "a95f304c2d6832d21c540acd0b86890bf60e5748"
+test_image_checksum "postgres:15.1" "4fa159437ff236e7cf2195a53e213e2f17f7b5d5"
 docker image rm "postgres:15.1"
-test_image_checksum "mongo:6.0.2" "fcebe9746c89d679947bcb731c33defd348e65c1"
+test_image_checksum "mongo:6.0.2" "11573dd2bb16683135ca303bb029ee0b1b1ae4a1"
 docker image rm "mongo:6.0.2"
-test_image_checksum "mariadb:10.9.4" "bbded79ab7977392228e666ec3bb0e43740b30a8"
+test_image_checksum "mariadb:10.9.4" "4b5aecbe74d0fbd6a68c68e832747e44e3a66768"
 docker image rm "mariadb:10.9.4"
-test_image_checksum "bitnami/kafka:3.3.1" "1d1a6188d413b25eb67bdc54de7d67a1114a4084"
+test_image_checksum "bitnami/kafka:3.3.1" "e6d488c86d9dfea31744f5f92c1c149c3735771a"
 docker image rm "bitnami/kafka:3.3.1"
-test_image_checksum "httpd:2.4.54" "da59d6fb2ed70ac1250e08bb6e6e972dd8ed6761"
+test_image_checksum "httpd:2.4.54" "e6d488c86d9dfea31744f5f92c1c149c3735771a"
 docker image rm "httpd:2.4.54"
-test_image_checksum "elasticsearch:8.5.0" "866e3c573f92b0dec18ee8ea29239bf097480e20"
+test_image_checksum "elasticsearch:8.5.0" "15fdb4ff7d4e8cdfddd18eeb73dbdb84dc99e003"
 docker image rm "elasticsearch:8.5.0"
-test_image_checksum "tomcat:9.0.69-jre17" "315a17e5bad8c975ca75083c6936817007bc54be"
+test_image_checksum "tomcat:9.0.69-jre17" "353d1be760e8a98d87b76521bd5d469d9f77f4c7"
 docker image rm "tomcat:9.0.69-jre17"
-test_image_checksum "redis:7.0.5" "59343e46bfc353ed046f9d3d713306850aff6d0e"
+test_image_checksum "redis:7.0.5" "80d3e29fc531047a40430194b6df1f0451db1fbc"
 docker image rm "redis:7.0.5"
-test_image_checksum "amazon/aws-otel-collector:v0.22.1" "70a41abc1e136b6151458c9249ae4f1daf7f8b2e"
+test_image_checksum "amazon/aws-otel-collector:v0.22.1" "72772ae3bc2632f8ee1a1af78c34f24bf23af444"
 docker image rm "amazon/aws-otel-collector:v0.22.1"
 
 # Git repository tests
 
-test_folder_checksum https://github.com/ethereum/remix-project.git 56a08b2d913355002087492781d008286b1348df fd4f279401567036cb33409fa54461ec27250f07
-test_folder_checksum https://github.com/jaegertracing/jaeger-operator.git 7e668d84b948b8366b46eaf5dfe0c0a849e943e4 0276abbd77fc62a8724f35297fe11c693a9fa787
-test_folder_checksum https://github.com/open-telemetry/opentelemetry-collector.git 7318c14f1a2b5a91d02171a0649be430cb27da94 57bb47c00ab976ad8b0a861bf60bd69fc51810ac
-test_folder_checksum https://github.com/prisma/prisma.git 98eb6ed30dd41d2978142f704b8caa4a0ed412f6 7a586c64e27db4b334d3735914cb96e081cbd192
-test_folder_checksum https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244 5fa75bfaab5f5adec5863ec18844fcef96b77e8a
+test_folder_checksum https://github.com/ethereum/remix-project.git 56a08b2d913355002087492781d008286b1348df 015b6f8d3d1379610e7a129bf3592360a2952035
+test_folder_checksum https://github.com/jaegertracing/jaeger-operator.git 7e668d84b948b8366b46eaf5dfe0c0a849e943e4 1ef86db5f550aa30dcddad32054ccac16a7af11f
+test_folder_checksum https://github.com/open-telemetry/opentelemetry-collector.git 7318c14f1a2b5a91d02171a0649be430cb27da94 427bfd7730daba621a96de38b5d589898021bad4
+test_folder_checksum https://github.com/prisma/prisma.git 98eb6ed30dd41d2978142f704b8caa4a0ed412f6 300630fe4876200aeaaf185dd25c24f170f7bc3f
+test_folder_checksum https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244 5492c5dd43b1b25bcd5c96a9e4e7058b7458274c
 
 rm -rf "$TEMP_DIR"
