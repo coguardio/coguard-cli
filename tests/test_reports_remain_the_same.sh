12d1cae52803c4e6006a5d16dd1ccb6dc0b27c29
Albert Heinle <albert.heinle@googlemail.com>
2023-11-02 22:45:34 -0400

    Integration test addition

--- tests/test_reports_remain_the_same.sh
+++ tests/test_reports_remain_the_same.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+test -n "$COGUARD_USER_NAME"
+test -n "$COGUARD_PASSWORD"
+
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 TEMP_DIR="$(mktemp -d)";
@@ -11,7 +14,7 @@ echo "Created temp directory $TEMP_DIR to store the temporary directory results.
 test_image_checksum() {
     IMAGE_NAME="$1";
     EXPECTED_CHECKSUM="$2"
-    ACTUAL_CHECKSUM=$( (cd "$SCRIPTPATH"/../src && python -m coguard_cli docker-image "$IMAGE_NAME") | tee "$TEMP_DIR/$IMAGE_NAME" | sort | shasum | awk '{print $1}' );
+    ACTUAL_CHECKSUM=$( (cd "$SCRIPTPATH"/../src && python -m coguard_cli --coguard-api-url https://test.coguard.io/server --coguard-auth-url https://test.coguard.io/auth docker-image "$IMAGE_NAME") | sed 1,18d | tee "$TEMP_DIR/$IMAGE_NAME" | sort | shasum | awk '{print $1}' );
     test "$ACTUAL_CHECKSUM" = "$EXPECTED_CHECKSUM"
     rm -rf "${TEMP_DIR:-?}/$IMAGE_NAME"
 }
@@ -22,42 +25,42 @@ test_folder_checksum() {
     EXPECTED_CHECKSUM="$3";
     git clone "$GIT_REPO" "$TEMP_DIR"/tmp_repo_dir;
     git -C "$TEMP_DIR"/tmp_repo_dir checkout "$GIT_HASH";
-    ACTUAL_CHECKSUM=$( (cd "$SCRIPTPATH"/../src && python -m coguard_cli folder "${TEMP_DIR:-?}"/tmp_repo_dir) | tee "$TEMP_DIR/folder_check.txt" | sort | shasum | awk '{print $1}' );
+    ACTUAL_CHECKSUM=$( (cd "$SCRIPTPATH"/../src && python -m coguard_cli --coguard-api-url https://test.coguard.io/server --coguard-auth-url https://test.coguard.io/auth folder "${TEMP_DIR:-?}"/tmp_repo_dir) | sed 1,18d | tee "$TEMP_DIR/folder_check.txt" | sort | shasum | awk '{print $1}' );
     test "$ACTUAL_CHECKSUM" = "$EXPECTED_CHECKSUM";
     rm -rf "${TEMP_DIR:-?}/tmp_repo_dir";
 }
 
 # Docker image tests
 
-test_image_checksum "nginx:1.23.2" "5609ecabe882db34bb5289c9f2247b7d26e04317"
+test_image_checksum "nginx:1.23.2" "4b3323a5333bc0063f5b49904530eb7b6b072252"
 docker image rm "nginx:1.23.2"
-test_image_checksum "mysql:8.0.31" "a705e1098b0c80f01245ede1f28ca50f1ffa53e1"
+test_image_checksum "mysql:8.0.31" "8a7b5ae7d69da636e4b12c7aefd9f6b376da22dd"
 docker image rm "mysql:8.0.31"
-test_image_checksum "postgres:15.1" "60edd380ed66af9de73ede2cbb3cdb6709700aa2"
+test_image_checksum "postgres:15.1" "8b1e1433a6ef1d79a40d39e87e94cc8cdefbaf60"
 docker image rm "postgres:15.1"
-test_image_checksum "mongo:6.0.2" "66a4a86216b943583115999c71d0ca6efffcafa7"
+test_image_checksum "mongo:6.0.2" "e7a1526b6a5896c5d663140a3182cc6715836397"
 docker image rm "mongo:6.0.2"
-test_image_checksum "mariadb:10.9.4" "fce461515b759275258c1f0c2b374d3a52144568"
+test_image_checksum "mariadb:10.9.4" "8526910fa23b027aaeeaecbc2a2c19096e9d8fb4"
 docker image rm "mariadb:10.9.4"
-test_image_checksum "bitnami/kafka:3.3.1" "1a397f87f4b1003801c40d499a655f0e631764e0"
+test_image_checksum "bitnami/kafka:3.3.1" "8e18871133667d2d58e634d9619607bc128dce52"
 docker image rm "bitnami/kafka:3.3.1"
-test_image_checksum "httpd:2.4.54" "3b2c2ae37e6039c7140ba5f69092bb9a0121dc6d"
+test_image_checksum "httpd:2.4.54" "87a1d6daba739fd65e06de46a3651fdb98188c48"
 docker image rm "httpd:2.4.54"
-test_image_checksum "elasticsearch:8.5.0" "652befcf31fd74713af9f5b29ecd27dfe108dba7"
+test_image_checksum "elasticsearch:8.5.0" "7637aee8084c39d1c1675d0e7f0748e296e63c49"
 docker image rm "elasticsearch:8.5.0"
-test_image_checksum "tomcat:9.0.69-jre17" "0c780f7503d5d44739073910dbf15d38e1a6c7d3"
+test_image_checksum "tomcat:9.0.69-jre17" "d10fa51c55b19b28c458a384b4b80e3f1471c04e"
 docker image rm "tomcat:9.0.69-jre17"
-test_image_checksum "redis:7.0.5" "770ca2a7ceea8ad78c51be4e2531bc546f0d077e"
+test_image_checksum "redis:7.0.5" "70a41abc1e136b6151458c9249ae4f1daf7f8b2e"
 docker image rm "redis:7.0.5"
-test_image_checksum "amazon/aws-otel-collector:v0.22.1" "360bf2d7e25e7e36185927568c13b8ec9d220e64"
+test_image_checksum "amazon/aws-otel-collector:v0.22.1" "3179369975c75e337f05c553281f5d5035cb1349"
 docker image rm "amazon/aws-otel-collector:v0.22.1"
 
 # Git repository tests
 
-test_folder_checksum https://github.com/ethereum/remix-project.git 56a08b2d913355002087492781d008286b1348df e55b6da66af6ab99a3763ddde2e85030290c7a6e
-test_folder_checksum https://github.com/jaegertracing/jaeger-operator.git 7e668d84b948b8366b46eaf5dfe0c0a849e943e4 57bc3dd5c81e20d04586e1c8b061e41f9f428cf5
-test_folder_checksum https://github.com/open-telemetry/opentelemetry-collector.git 7318c14f1a2b5a91d02171a0649be430cb27da94 300d39e308b550caaa0d8597592b0d385b37b126
-test_folder_checksum https://github.com/prisma/prisma.git 98eb6ed30dd41d2978142f704b8caa4a0ed412f6 52d38b49a6673addf432d3a9b86ca18e83346a72
-test_folder_checksum https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244 66c847b61daf597967d00a4c5bacbec587511d82
+test_folder_checksum https://github.com/ethereum/remix-project.git 56a08b2d913355002087492781d008286b1348df 31b4c7c9b7a9a29fd9d8a3acaf25a4a53c449261
+test_folder_checksum https://github.com/jaegertracing/jaeger-operator.git 7e668d84b948b8366b46eaf5dfe0c0a849e943e4 ed4a13cbc4fb4e29342cdac64d49d64d31b3efd6
+test_folder_checksum https://github.com/open-telemetry/opentelemetry-collector.git 7318c14f1a2b5a91d02171a0649be430cb27da94 3c092bdd8357c717e7966f608c62c97ec0cb78f4
+test_folder_checksum https://github.com/prisma/prisma.git 98eb6ed30dd41d2978142f704b8caa4a0ed412f6 2bc474fc32f4ff6cb0fb51bee6069f8a276fb98e
+test_folder_checksum https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244 a0fa0659983a5639177c6e951aa3da535e64bd0f
 
 rm -rf "$TEMP_DIR"
