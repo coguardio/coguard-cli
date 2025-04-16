7f2530499ed51936036c5eba4dbd83d2713e9bdd
Albert Heinle <albert.heinle@googlemail.com>
2025-04-16 00:31:21 -0400

    Adapted automation tests

--- tests/test_reports_remain_the_same.sh
+++ tests/test_reports_remain_the_same.sh
@@ -76,41 +76,41 @@ test_folder_fix() {
 
 # Docker image tests
 
-test_image_checksum "nginx:1.23.2" "ca532b2cc57393c82504333a5dad1c947afa83be"
+test_image_checksum "nginx:1.23.2" "fa46fc577dafcb4684287a4854102a12f90dd940"
 docker image rm "nginx:1.23.2"
-test_image_checksum "mysql:8.0.31" "15f9d4d4a0c1356ad22260793c97789825ecc00d"
-test_image_checksum "mysql:8.0.31" "ffd0dfbf8d5a12436cef8639fdbb587fc989afcd" stig
+test_image_checksum "mysql:8.0.31" "dda34193c1d3213626bc1a5f155c380eb5cbd323"
+test_image_checksum "mysql:8.0.31" "d65610214bb17cbf3b9c65f3a2467acdd0a7857c" stig
 docker image rm "mysql:8.0.31"
-test_image_checksum "postgres:15.1" "87eb693e99e9cdd60f5f8d79cea4d75fa2675ed9"
-test_image_checksum "postgres:15.1" "f4333d5bc08540be3b17ce7c9430a4fef78437ab" soc2
+test_image_checksum "postgres:15.1" "2ff64048c2bfec0b486923b7f3f4696f079fc333"
+test_image_checksum "postgres:15.1" "b9ee3668416f2241e75e29bc2aa263898c56d52f" soc2
 docker image rm "postgres:15.1"
-test_image_checksum "mongo:6.0.2" "be2d4a8ee7c83dbd04a896f44fd6ac5f33b69913"
+test_image_checksum "mongo:6.0.2" "ed1b781f6c4766492f5dfec9a08911103d4a7709"
 docker image rm "mongo:6.0.2"
-test_image_checksum "mariadb:10.9.4" "1498f771142810f494930da77234a0b6c5b59e6b"
-test_image_checksum "mariadb:10.9.4" "54318b75d6fc2f7ec948fdb64d63c82ff0ff20b4" hipaa
+test_image_checksum "mariadb:10.9.4" "5e02a7ee3af81d441a4618cb64013024439e772e"
+test_image_checksum "mariadb:10.9.4" "7c3b9cf5dff36df9c4f4a1a6411c2a7731542bd8" hipaa
 docker image rm "mariadb:10.9.4"
-test_image_checksum "bitnami/kafka:3.3.1" "9dae7d5bfc8d9f9002e3fc670c941092c4c14f56"
+test_image_checksum "bitnami/kafka:3.3.1" "a002a4c5a0319e0ddfbd0c6c565a1fdd1848c811"
 docker image rm "bitnami/kafka:3.3.1"
-test_image_checksum "httpd:2.4.54" "774139309ef3ff82f40f5c31a2b6e7493da5ea07"
+test_image_checksum "httpd:2.4.54" "f906d9ed32513540a12d73603b98b8678742d59e"
 docker image rm "httpd:2.4.54"
-test_image_checksum "elasticsearch:8.5.0" "54d3970c4ac30423e9e92beb2356246adddb0246"
+test_image_checksum "elasticsearch:8.5.0" "2138601d688615ad6ac277e2de1bb07fb0176a36"
 docker image rm "elasticsearch:8.5.0"
-test_image_checksum "tomcat:9.0.69-jre17" "712d6a7502e64cb9baa00e0571cb20c89ebf4c3b"
+test_image_checksum "tomcat:9.0.69-jre17" "23bd82b42026bbd427c3e76747e70bd41be70be5"
 docker image rm "tomcat:9.0.69-jre17"
-test_image_checksum "redis:7.0.5" "32ebbd865d23088b802ea5adee2b068734f9d6ca"
+test_image_checksum "redis:7.0.5" "55afff4ee9dc3c807550a68ed332e035d388273a"
 docker image rm "redis:7.0.5"
 test_image_checksum "rethinkdb:2.4.4-bookworm-slim" "728e5bff42a891911520e394a04a11f05439159a"
 docker image rm "rethinkdb:2.4.4-bookworm-slim"
-test_image_checksum "amazon/aws-otel-collector:v0.22.1" "9253ac064181113f972856a7f304fb7cc9d21ae6"
+test_image_checksum "amazon/aws-otel-collector:v0.22.1" "9a8076467bd2e789f61c72a828633b29477bdca9"
 docker image rm "amazon/aws-otel-collector:v0.22.1"
 
 # Git repository tests
 
-test_folder_checksum https://github.com/ethereum/remix-project.git 56a08b2d913355002087492781d008286b1348df cff2be9fa9933ecdbf05fbac3b6530d0364e68c0
-test_folder_checksum https://github.com/jaegertracing/jaeger-operator.git 7e668d84b948b8366b46eaf5dfe0c0a849e943e4 0867ade8961470de6dadb31e88bf7b3c381a0c05
-test_folder_checksum https://github.com/open-telemetry/opentelemetry-collector.git 7318c14f1a2b5a91d02171a0649be430cb27da94 6f7ef1a03ae3c0234580a3cbc7ca672f54bd959c
-test_folder_checksum https://github.com/prisma/prisma.git 98eb6ed30dd41d2978142f704b8caa4a0ed412f6 665ccf1f9c8a3f17c6f85646db6a3868014ad195
-test_folder_checksum https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244 d5eeca2c63c658fb9fc3df6f096658b14b00a737
+test_folder_checksum https://github.com/ethereum/remix-project.git 56a08b2d913355002087492781d008286b1348df e3c991f1c2cc98a0af2f50913ea764ecc38d4d22
+test_folder_checksum https://github.com/jaegertracing/jaeger-operator.git 7e668d84b948b8366b46eaf5dfe0c0a849e943e4 b90fd3d318af7f0cb3c9a134681733a34ddbc63e
+test_folder_checksum https://github.com/open-telemetry/opentelemetry-collector.git 7318c14f1a2b5a91d02171a0649be430cb27da94 5ab34c83e55232b65d09458c406030def9a8b058
+test_folder_checksum https://github.com/prisma/prisma.git 98eb6ed30dd41d2978142f704b8caa4a0ed412f6 d556d05bdb095d2278a450240ae92e7f384520ac
+test_folder_checksum https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244 be34aedb726dcf8b82fef179d804d0de592fbca2
 test_folder_fix https://github.com/zabbix/zabbix.git 3cbf261947d2b4148dd6a29dfcf5b1a15a857244
 
 # Test with a weird name
