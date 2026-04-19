08c0de9222705270fca23429630ebae50b50c7f6
Albert Heinle <albert.heinle@googlemail.com>
2026-04-19 18:11:54 -0400

    iso27001 - Final hash

--- tests/test_reports_remain_the_same.sh
+++ tests/test_reports_remain_the_same.sh
@@ -115,7 +115,7 @@ time test_image_checksum "mariadb:10.9.4" "f95e6591b09610a75f50e3967f7c7abf1525d
 time test_image_checksum "mariadb:10.9.4" "797845ab3a1b74f0ae38fbd63c02ac41613024e9" hipaa
 time test_image_checksum "mariadb:10.9.4" "a6d375e07bfa0d16b2b02e6eee7323bc218bc570" nist800-53
 time test_image_checksum "mariadb:10.9.4" "7292bb041c90962ccfe737ea2b8c18cf3686c894" fedramp
-time test_image_checksum "mariadb:10.9.4" "d8cc82a676d7f19cd5f1dadd84914b0ddc94c2d1" iso27001
+time test_image_checksum "mariadb:10.9.4" "2af133f8352ab118f5a96fb2e447dd545110723e" iso27001
 time docker image rm "mariadb:10.9.4"
 time test_image_checksum "bitnamilegacy/kafka:3.3.1" "b15fa38b8a05564ae5ecb52705c14b1a036ec426"
 time docker image rm "bitnamilegacy/kafka:3.3.1"
